ea6a42b5ec4f47a88bf81140040a600892566374
ROHIT KUMAR GOYAL <goyal.rohit.14012000@gmail.com>
2022-06-12 16:09:31 +0530

    first commit

---
+++ selection.js
@@ -0,0 +1,41 @@
+async function selection(){
+    console.log('In selection()');
+    const ele = document.querySelectorAll(".bar");
+    for(let i = 0; i < ele.length; i++)
+    {
+        let min_index = i;
+        ele[i].style.background = 'blue';
+        for(let j = i+1; j < ele.length; j++)
+        {
+            ele[j].style.background = 'red';
+
+            await waitforme(delay);
+            if(parseInt(ele[j].style.height) < parseInt(ele[min_index].style.height))
+            {
+                console.log('In if condition height comparision');
+                if(min_index !== i){
+                    ele[min_index].style.background = 'cyan';
+                }
+                min_index = j;
+            } 
+            else
+            {
+                ele[j].style.background = 'cyan';
+            }   
+        }
+        await waitforme(delay);
+        swap(ele[min_index], ele[i]); 
+        ele[min_index].style.background = 'cyan';
+        ele[i].style.background = 'green';
+    }
+}
+const selectionSortbtn = document.querySelector(".selectionSort");
+selectionSortbtn.addEventListener('click', async function(){
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    await selection();
+    enableSortingBtn();
+    enableSizeSlider();
+    enableNewArrayBtn();
+});
